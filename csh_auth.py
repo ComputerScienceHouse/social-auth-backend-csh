4036739c36a87c4833352279849331bf8df391b1
Marc Billow <mbillow@users.noreply.github.com>
2018-02-05 22:29:57 -0500

    Update SSO Route

--- csh_auth.py
+++ csh_auth.py
@@ -6,5 +6,5 @@ from social-auth-core.backends.open_id_connect import OpenIdConnectAuth
 
 class CSHAuth(OpenIdConnectAuth):
     name = 'csh'
-    OIDC_ENDPOINT = 'https://sso.csh.rit.edu/realms/csh'
+    OIDC_ENDPOINT = 'https://sso.csh.rit.edu/auth/realms/csh'
 
